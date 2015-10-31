8379067672695f4c19806e061cd86091ad2071a5
Aislan Dener Souza Vicentini <aislandenersouzavicentini@gmail.com>
2015-10-30 22:03:18 -0200

    Criando as Roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    case user.has_role?
+    when :gerente
+      can :manager, :all
+    end
+    when :conta_pagar
+      can :manager,[ContasPagar,Cliente]
+      can :read,[Fornecedor]
+    end
+    when :conta_receber
+      can :manager,[ContaReceber,Fornecedor]
+      can :read,[Cliente]
+    end
+
+
   end
 end
